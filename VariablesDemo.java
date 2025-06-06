688146c7a9b08d64e94dcd79424250235d4a3f2d
ravi2krishna <ravi2krishna@gmail.com>
2025-06-06 13:08:59 +0530

    Data Types In Java

--- VariablesDemo.java
+++ VariablesDemo.java
@@ -1,10 +1,26 @@
 public class VariablesDemo {
     public static void main(String[] args) {
         // comments which are ignored by compiler
-        // define variables
-        int age = 20;
+        // define variables with data types
+        byte byteNumber = 100;
+        short shortNumber = 1000;
+        int intNumber = 20;
+        int longNumber = 20000;
+        float floatNumber = 3.5f;
+        double doubleNumber = 3.14;
+        boolean booleanValue = true; // false
+        char charValue = 'A';
+
         String name = "Ravi";
         String message = "Welcome To Java Programming";
         System.out.println(message);
+        System.out.println(byteNumber);
+        System.out.println(shortNumber);
+        System.out.println(intNumber);
+        System.out.println(longNumber);
+        System.out.println(floatNumber);
+        System.out.println(doubleNumber);
+        System.out.println(booleanValue);
+        System.out.println(charValue);
     }
 }
