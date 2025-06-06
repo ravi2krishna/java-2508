8d51dc17b01bdfedfbb0e28796e3d43f5d9ee3c5
ravi2krishna <ravi2krishna@gmail.com>
2025-06-06 12:46:44 +0530

    variables demo

---
+++ VariablesDemo.java
@@ -0,0 +1,10 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        // comments which are ignored by compiler
+        // define variables
+        int age = 20;
+        String name = "Ravi";
+        String message = "Welcome To Java Programming";
+        System.out.println(message);
+    }
+}
