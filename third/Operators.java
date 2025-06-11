a98ecff25559dbed19e4d65f10203acffe59c90c
ravi2krishna <ravi2krishna@gmail.com>
2025-06-11 11:54:46 +0530

    Arithmetic Ops

--- third/Operators.java
+++ third/Operators.java
@@ -1,5 +1,27 @@
 package third;
 
 public class Operators {
-    
+    public static void main(String[] args) {
+        // Arithmetic Ops
+        int num1 = 10;
+        int num2 = 5;
+
+        // Maths Cal
+        // Addition + 
+        System.out.println("Sum is: "+(num1+num2));
+
+        // Sub + 
+        System.out.println("Diff is: "+(num2-num1));
+
+        // Product * 
+        System.out.println("Product is: "+(num1*num2));
+        
+        // Division / 
+        System.out.println("Divi is: "+(num1/num2));
+
+        // Modulus %
+        System.out.println("Mod is: "+(num1%num2));
+
+        
+    }
 }
