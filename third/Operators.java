918511829fd79e4c13e75b60ed04425babe17102
ravi2krishna <ravi2krishna@gmail.com>
2025-06-11 11:59:18 +0530

    Increment - Decrement Ops

--- third/Operators.java
+++ third/Operators.java
@@ -22,6 +22,14 @@ public class Operators {
         // Modulus %
         System.out.println("Mod is: "+(num1%num2));
 
-        
+        // Increment - Decrement
+        int a = 10;
+        System.out.println("Original A Value: "+a);
+        System.out.println("++A Value: "+ ++a); // pre
+        System.out.println("A++ Value: "+ a++); // post
+        System.out.println("Updated A Value: "+a);
+
+        // Try Out with - Increment
+
     }
 }
