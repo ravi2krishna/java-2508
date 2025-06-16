530ed002c6bf40bf244089b324593169dd8b78bf
ravi2krishna <ravi2krishna@gmail.com>
2025-06-16 12:13:23 +0530

    do-while demo

---
+++ fifth/DoWhileDemo.java
@@ -0,0 +1,10 @@
+package fifth;
+
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int pin = 1235;
+        do {
+            System.out.println("Welcome To SBI ATM");
+        } while (pin == 1234);
+    }
+}
