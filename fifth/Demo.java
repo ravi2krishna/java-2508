fe3b9cb3e8613f9c0c4e33761a0e22ec0b3ef41a
ravi2krishna <ravi2krishna@gmail.com>
2025-06-16 12:04:46 +0530

    continue keyword in java

--- fifth/Demo.java
+++ fifth/Demo.java
@@ -14,5 +14,16 @@ public class Demo {
         System.out.println("Hi");
         System.out.println("Hi");
         System.out.println("Hi");
+
+            // fill 5 water bottles
+        for(int i=1;i<=5;i++) {
+            // skip  bottle 3, it's broken
+            // continue -> skip that specific condition
+            if (i == 3) {
+                continue;
+            }
+            
+            System.out.println("Filling Water Bottle: "+i);
+        }
     }
 }
