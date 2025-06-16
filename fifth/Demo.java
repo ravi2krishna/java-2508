fbb6ef89eb1170bd0fa763c4df01ad37c0574e24
ravi2krishna <ravi2krishna@gmail.com>
2025-06-16 11:55:43 +0530

    for demo

---
+++ fifth/Demo.java
@@ -0,0 +1,18 @@
+package fifth;
+
+public class Demo {
+    public static void main(String[] args) {
+        // without Loops
+        // Say Hi For 10 Times
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+    }
+}
---
+++ fifth/ForDemo.java
@@ -0,0 +1,14 @@
+package fifth;
+
+public class ForDemo {
+    public static void main(String[] args) {
+        for (int i=0;i<=10;i++) {
+            System.out.println("Hi");
+        }
+
+        // fill 5 water bottles
+        for(int i=1;i<=5;i++) {
+            System.out.println("Filling Water Bottle: "+i);
+        }
+    }
+}
