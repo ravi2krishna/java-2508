663b3fbb7fc38adca19bb19f6b5a4817ecaec5cf
ravi2krishna <ravi2krishna@gmail.com>
2025-06-23 12:40:05 +0530

    constructor overloading

--- eight/Test.java
+++ eight/Test.java
@@ -5,6 +5,8 @@ public class Test {
     // instance variable
     int x;
     int y;
+    int z ;
+    double a;
 
     // explicit constructor without parameters -> what values you wanted, you defined
     public Test () {
@@ -19,8 +21,30 @@ public class Test {
      this.y = y;   
     }
 
+    // number of parameters changed
+    public Test (int x, int y, int z) {
+        // the above x & y are Local Variables
+     this.x = x;
+     this.y = y;   
+     this.z = z;
+    }
+
+    // type of parameters changed
+    public Test (int x, double a) {
+        // the above x & y are Local Variables
+     this.x = x;
+     this.a = a;    
+    }
+
+    // order of parameters changed
+    public Test (double a, int x) {
+        // the above x & y are Local Variables
+     this.x = x;
+     this.a = a;    
+    }
+
     public int addNumbers () {
-        return x+y;
+        return x+y+z;
     }
 
     public static void main(String[] args) {
@@ -31,6 +55,9 @@ public class Test {
 
         Test t2 = new Test(10,20);
         System.out.println("sum of values from parametrised "+t2.addNumbers());
+
+        Test t3 = new Test(10,20,30);
+        System.out.println("sum of values from parametrised "+t3.addNumbers());
     }
 
 }
