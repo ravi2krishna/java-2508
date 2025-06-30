871e95c8f5547f36e0ce54e1056dc4ad640b788a
ravi2krishna <ravi2krishna@gmail.com>
2025-06-30 11:44:44 +0530

    Issue with Strings

--- ten/ImmutablityTest.java
+++ ten/ImmutablityTest.java
@@ -6,6 +6,11 @@ public class ImmutablityTest {
         String str = "Hello";
         System.out.println(str.concat(" World"));
         System.out.println(str);
-        
+
+        String s = "hello";
+        s = s.concat("world"); // every change we made -> new object is created
+        s = s.concat("Java"); //  new object is created
+        s = s.concat("Program"); // new object is created
+
     }
 }
