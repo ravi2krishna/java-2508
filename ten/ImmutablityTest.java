21fc2484546bf7fbbc32dc52262082fc4ae842de
ravi2krishna <ravi2krishna@gmail.com>
2025-06-30 11:39:54 +0530

    String Immutablity Test

---
+++ ten/ImmutablityTest.java
@@ -0,0 +1,11 @@
+package ten;
+
+public class ImmutablityTest {
+    public static void main(String[] args) {
+        
+        String str = "Hello";
+        System.out.println(str.concat(" World"));
+        System.out.println(str);
+        
+    }
+}
