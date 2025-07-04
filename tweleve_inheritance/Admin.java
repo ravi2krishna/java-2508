154855e2f020af79b99f80e444daef38d11301fb
ravi2krishna <ravi2krishna@gmail.com>
2025-07-04 11:44:07 +0530

    Multi Level Inheritance

--- tweleve_inheritance/Admin.java
+++ tweleve_inheritance/Admin.java
@@ -1,31 +1,33 @@
 package tweleve_inheritance;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.awt.Desktop;
+// import java.io.IOException;
+// import java.net.URI;
+// import java.net.URISyntaxException;
+// import java.awt.Desktop;
 
-public class Admin {
+// Multi Level Inheritance
+// Guest -> VideoAdmin -> Admin ....
+public class Admin extends VideoAdmin {
 
     // watch video
-    public void watchVideos() {
-      System.out.println("Can Watch Videos");
-      System.out.println("Opening video in browser...");
-      try {
-          // Check if Desktop API is supported
-          if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-              Desktop.getDesktop().browse(new URI("https://vimeo.com/449787858"));
-          } else {
-              System.out.println("Desktop not supported. Cannot open browser.");
-          }
-      } catch (IOException | URISyntaxException e) {
-          System.out.println("Error: " + e.getMessage());
-      }
-    }
-    // add video
-    public void addVideos() {
-        System.out.println("Can Add Videos");
-    }
+    // public void watchVideos() {
+    //   System.out.println("Can Watch Videos");
+    //   System.out.println("Opening video in browser...");
+    //   try {
+    //       // Check if Desktop API is supported
+    //       if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+    //           Desktop.getDesktop().browse(new URI("https://vimeo.com/449787858"));
+    //       } else {
+    //           System.out.println("Desktop not supported. Cannot open browser.");
+    //       }
+    //   } catch (IOException | URISyntaxException e) {
+    //       System.out.println("Error: " + e.getMessage());
+    //   }
+    // }
+    // // add video
+    // public void addVideos() {
+    //     System.out.println("Can Add Videos");
+    // }
 
     // delete video
     public void deleteVideos() {
