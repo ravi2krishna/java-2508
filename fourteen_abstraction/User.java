a91916fa9f05c38a208e3356d065335a69501525
ravi2krishna <ravi2krishna@gmail.com>
2025-07-09 12:51:51 +0530

    Abstraction achieved using Interfaces

--- fourteen_abstraction/User.java
+++ fourteen_abstraction/User.java
@@ -2,11 +2,13 @@ package fourteen_abstraction;
 
 public class User {
     public static void main(String[] args) {
-        Lenovo lenovo = new Lenovo();
+        // Abstraction
+        Laptop lenovo = new Lenovo();
         lenovo.usbA();
         lenovo.usbC();
         lenovo.hdmi();
         
+        // No-Abstraction
         HP hp = new HP();
         hp.usbA();
         hp.usbC();
